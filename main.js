6f7e0d79235f4db56145623c4b4bfd6b49b558c9
devkiloton <dev.kiloton@gmail.com>
2022-09-07 10:30:28 -0300

    Basic transition

--- main.js
+++ main.js
@@ -1,27 +1,23 @@
 window.onload = () => {
-  const anchors = document.querySelectorAll('a');
-  const transition_el = document.querySelector('.transition');
+  const transition_el = document.querySelector(".transition");
+  const anchors = document.querySelectorAll("a");
 
   setTimeout(() => {
-    transition_el.classList.remove('is-active');
+    transition_el.classList.remove("is-active");
   }, 500);
 
   for (let i = 0; i < anchors.length; i++) {
     const anchor = anchors[i];
 
-    anchor.addEventListener('click', e => {
+    anchor.addEventListener("click", (e) => {
       e.preventDefault();
       let target = e.target.href;
 
-      console.log(transition_el);
+      transition_el.classList.add("is-active");
 
-      transition_el.classList.add('is-active');
-
-      console.log(transition_el);
-
-      setInterval(() => {
+      setTimeout(() => {
         window.location.href = target;
       }, 500);
-    })
+    });
   }
-}
+};
